2c71fca715b51e45152f718b32e265dcd9d7eaad
StastnyJ <xstastnyj@gmail.com>
2020-10-01 13:39:30 +0200

    basic message

--- public/lib/js/themeProvider.js
+++ public/lib/js/themeProvider.js
@@ -2,7 +2,7 @@ function GTheme() {
   this.get = () => localStorage.getItem("theme") || "light";
 
   this.apply = () => {
-      const theme = GTheme.get();
+      const theme = new GTheme().get();
       if (theme === "dark") {
           document.body.style.setProperty("--theme-primary", "#212121");
           document.body.style.setProperty("--theme-contrast", "#ffffff");
