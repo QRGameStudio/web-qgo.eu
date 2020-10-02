126035282684c521815dd8114a2a1ffef8292570
StastnyJ <xstastnyj@gmail.com>
2020-10-02 15:01:52 +0200

    basic message

--- src/Utils/gameCodeBuilder.ts
+++ src/Utils/gameCodeBuilder.ts
@@ -1,5 +1,5 @@
 const libs = {
-  js: ["jquery.min.js", "bootstrap.min.js", "GStorage.js", "musicComposer.js", "themeProvider.js"],
+  js: ["jquery.min.js", "bootstrap.min.js", "GStorage.js", "musicComposer.js", "themeProvider.js", "modals.js"],
   css: ["bootstrap.min.css", "themeProvider.css"],
 };
 
