eef66f7b26f5b8822df27061d0b98ad4648b96a5
StastnyJ <xstastnyj@gmail.com>
2020-10-02 12:58:32 +0200

    basic message

--- public/lib/js/GStorage.js
+++ public/lib/js/GStorage.js
@@ -1,10 +1,11 @@
-function GStorage(context, temporary=false) {
-    const storageProvider = temporary ? sessionStorage : localStorage;
+function GStorage(context, temporary = false) {
+  const storageProvider = temporary ? sessionStorage : localStorage;
 
-    this.get = (key, def=null) => Promise.resolve(JSON.parse(storageProvider.getItem(`${context}/${key}`) || "null") || def);
-    this.set = (key, value) => new Promise((r) => {
-        storageProvider.setItem(`${context}/${key}`, JSON.stringify(value));
-        r(true);
+  this.get = (key, def = null) => Promise.resolve(JSON.parse(storageProvider.getItem(`${context}/${key}`) || "null") || def);
+  this.set = (key, value) =>
+    new Promise((r) => {
+      storageProvider.setItem(`${context}/${key}`, JSON.stringify(value));
+      r(true);
     });
-    this.del = (key) => this.set(key, null);
+  this.del = (key) => this.set(key, null);
 }
--- public/lib/js/themeProvider.js
+++ public/lib/js/themeProvider.js
@@ -2,13 +2,15 @@ function GTheme() {
   this.get = () => localStorage.getItem("theme") || "light";
 
   this.apply = () => {
-      const theme = new GTheme().get();
-      if (theme === "dark") {
-          document.body.style.setProperty("--theme-primary", "#212121");
-          document.body.style.setProperty("--theme-contrast", "#ffffff");
-      }else{
-        document.body.style.setProperty("--theme-primary", "#ffffff");
-        document.body.style.setProperty("--theme-contrast", "#000000");
+    const theme = new GTheme().get();
+    if (theme === "dark") {
+      document.body.style.setProperty("--theme-primary", "#212121");
+      document.body.style.setProperty("--theme-contrast", "#ffffff");
+    } else {
+      document.body.style.setProperty("--theme-primary", "#ffffff");
+      document.body.style.setProperty("--theme-contrast", "#000000");
     }
-  }
+  };
 }
+
+new GTheme().apply();
